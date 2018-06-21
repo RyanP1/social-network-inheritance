bdfc1b74803ae14a7d1ef40a9ac7777e55a64e74
Peter Ryan <peter.e.ryan@gmail.com>
2018-06-21 20:31:49 +0000

    social network project finished

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,28 @@
+from datetime import datetime
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
+        
     def add_post(self, post):
-        pass
-
+        post.set_user(self)
+        self.posts.append(post)
+    
     def get_timeline(self):
-        pass
+        timeline = []
+        for user in self.following:
+            for p in user.posts:
+                timeline.append(p)
+        
+        return sorted(timeline, key = lambda p: p.timestamp, reverse=False)
+        
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
